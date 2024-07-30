b18a74e7b22c42349ca33bb3ac617d32c586ce59
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-07-31 01:46:07 +0600

    udpated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer
-      className="absolute bottom-0 w-full bg-center bg-no-repeat bg-cover"
-      style={{ backgroundImage: "url(/banner3.png)", height: "556px" }}
+      className="absolute bottom-0 w-full bg-center bg-no-repeat bg-cover h-[200px] lg:h-[556px] "
+      style={{ backgroundImage: "url(/banner3.png)" }}
     >
       <div className="flex items-end justify-center h-full pb-4 mb-4 md:pl-40 lg:pl-80">
         <button className="px-4 py-2 border-2 rounded-md bg-[#5bb2ff] text-white flex items-center gap-0.5 border-black border-b-4">
